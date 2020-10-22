4ecf7ef26c2a9fb0873d956aaa3c749acce463b0
“Rui <rui.vieira.vv@gmail.com>
2020-10-22 16:46:27 +0100

    __name__ main

---
+++ gitCommit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git config --global user.name “Rui Vieira”
+git config --global user.email “rui.vieira.vv@gmail.com”
+
+git add gitCommit.sh
+git commit -m “add .sh file”
+git push
+
+# RuiVieira89
