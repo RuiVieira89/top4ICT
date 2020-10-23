de8d2bdf887d183658363a214b8014374c5d7612
“Rui <rui.vieira.vv@gmail.com>
2020-10-23 08:53:34 +0100

    coding functions

---
+++ function.py
@@ -0,0 +1,52 @@
+
+
+import numpy as np
+
+def function(x):
+
+	return x
+
+def Encurvadura(x):
+
+	'''Encurvadura'''
+	l = x[0] # 0.4 # heigth [m]
+	e = x[1] # 0.001 # force eccentricity[m], 0 if none 
+	P = 10 # force [N]
+	E = 117e9 # elastcity module ·[Pa] (copper)
+	r = 1/100 # [m]
+	I = (np.pi/4)*r**4
+	k = np.sqrt(np.abs(P)/(E*I))
+
+	flexa = e*(1-np.cos(k*l))/np.cos(k*l) # flexion [m]
+	Mf_max = P*(e+flexa) # maximum moment of flextion
+	W = l*(np.pi*r**2) # volume [mn]
+	tension_max = P/(np.pi*r**2) + Mf_max/W
+
+
+	return tension_max
+
+
+def platesTheory():
+	# https://en.wikipedia.org/wiki/Kirchhoff%E2%80%9nLove_plate_theory
+	# Isotropic quasistatic Kirchhoff-Love plates
+
+	ne = 0.355 # Poisson's ratio (copper)
+	E = 117e9 # elastcity module ·[Pa] (copper)
+	n = 3
+
+	sigma = np.zeros(n) # tension
+	sigma_old = np.zeros(n)
+	strain = np.zeros(n) # strain
+
+	# material matrix
+	C = np.eye(3) 
+	C[0,1] = C[0,1] = ne
+	C[-1,-1] = 1 - ne
+	C *= E/(1-ne**2)
+
+	cond = False
+	while cond is False:
+		sigma = np.linalg.solve(C,strain)
+		print(sigma)
+
+		cond = sigma - sigma_old < 1e6
---
+++ plotSurf.py
@@ -0,0 +1,38 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import minimize
+
+from function import function
+
+def plotSurf(A, minimum):
+
+
+	delta = 0.1
+	G = 10
+	x = y = np.arange(0, G, delta)
+	X,Y = np.meshgrid(x, y)
+	Z = function([X, Y])
+
+	plt.ion()
+	fig = plt.figure(1, figsize=(10,8), clear=True)
+	ax = fig.add_subplot(1, 1, 1)
+
+	im = ax.contourf(X, Y, Z, levels=100)
+
+	if A != []:
+		ax.scatter(A[:,0], A[:,1], marker='x', c='r')
+
+		for i in range(0,A.shape[0]):
+			s = f"X{i}"
+			ax.text(A[i,0], A[i,1], s, fontsize=12)
+
+	if minimum != []:
+		s1 = 'Min'
+		ax.scatter(minimum.x[0], minimum.x[1], marker='o', c='r')
+		ax.text(minimum.x[0], minimum.x[1], s1, fontsize=12)
+
+
+	fig.colorbar(im,ax=ax)
+
+	plt.pause(0.1)#(0.0001)
